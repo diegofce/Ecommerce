1941c57f798bdf40c96b7c3ddaa3f68f1af863a8
Diego Chacon <chacondiego31@gmail.com>
2024-07-02 10:43:51 -0500

    cambios en el boton visualizar

--- src/app/pages/products/product/product.component.ts
+++ src/app/pages/products/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, Input, } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Product } from '../../../api/model/product';
